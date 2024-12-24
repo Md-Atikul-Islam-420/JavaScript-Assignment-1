c873e27f64fab2842e8023adc69260f88d5d144b
Md-Atikul-Islam-420 <mdatikulislam2314@gmail.com>
2024-12-25 00:23:45 +0600

    Add task function

--- index.js
+++ index.js
@@ -3,3 +3,14 @@
 const todoInput = document.getElementById('todo-input');
 const addBtn = document.getElementById('add-btn');
 const todoList = document.getElementById('todo-list');
+
+// Add task function
+addBtn.addEventListener('click', () => {
+    const taskText = todoInput.value.trim();
+    if (taskText === '') {
+      alert('Please enter a value!');
+      return;
+    }
+    // addTask(taskText);
+    // todoInput.value = ''; // Clear the input field
+  });
